71269a307d176bffe86dc722a4126671fd768f9b
Manuel-kl <mannuehkipkirui@gmail.com>
2023-08-26 23:26:15 +0300

    Added _redirects file, to fix the netlify not found issue

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const accessToken = process.env.VUE_APP_UNSPLASH_API_KEY;
-
+console.log(accessToken);
 export const unsplashApi = axios.create({
   baseURL: "https://api.unsplash.com",
 
